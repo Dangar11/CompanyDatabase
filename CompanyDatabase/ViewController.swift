831a593aa06b90dac429e1858cfda75a60e2df61
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-11-10 18:18:24 +0200

    initial project

---
+++ CompanyDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompanyDatabase
+//
+//  Created by Igor Tkach on 10.11.2019.
+//  Copyright © 2019 Igor Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
