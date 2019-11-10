5ed46e92e851c7fe4e1a09efd0201c8183255fbd
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-11-10 21:32:05 +0200

    Style navigation bar for main screen, change all template code for SceneKit in AppDelegate

---
+++ CompanyDatabase/CustomNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  CompanyDatabase
+//
+//  Created by Igor Tkach on 10.11.2019.
+//  Copyright © 2019 Igor Tkach. All rights reserved.
+//
+
+import UIKit
+
+
+class CustomNavigationController: UINavigationController {
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+}
--- CompanyDatabase/ViewController.swift
+++ CompanyDatabase/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .white
+    
+    navigationItem.title = "Companies"
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleCompany))
+    
+    setupNavigationStyle()
+    
+    
+  }
+  
+  
+  // MARK: - Set Navigation Style
+  func setupNavigationStyle() {
+    
+    navigationController?.navigationBar.isTranslucent = false
+    navigationController?.navigationBar.prefersLargeTitles = true
+    navigationController?.view.backgroundColor = UIColor.chinaRose
+    
+    navigationController?.navigationBar.largeTitleTextAttributes =
+      [NSAttributedString.Key.foregroundColor: UIColor.white]
+    
   }
 
+  
+  @objc func handleCompany() {
+    
+  }
+  
+  
+  
 
 }
+
+
+
 
